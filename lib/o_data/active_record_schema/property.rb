8cf18e185ea1db270e66a4d5d49cf84630ce8fc4
Sebastian Kliem <sebastian-kliem@live.de>
2016-06-09 14:33:45 +0200

    fix - format DateTime properties compliant to the OData standard (without tailng zone adjustment)

--- lib/o_data/active_record_schema/property.rb
+++ lib/o_data/active_record_schema/property.rb
@@ -38,7 +38,7 @@ module OData
       
       def value_for(one)
         v = one.send(@column_adapter.name.to_sym)
-        return v.to_datetime.iso8601 if v.class == ActiveSupport::TimeWithZone
+        return v.to_datetime.strftime('%Y-%m-%dT%H:%M:%S') if v.class == ActiveSupport::TimeWithZone
         v.respond_to?(:iso8601) ? v.send(:iso8601) : v
       end
     end
--- lib/o_data/version.rb
+++ lib/o_data/version.rb
@@ -1,3 +1,3 @@
 module OData
-  VERSION = "0.0.15"
+  VERSION = "0.0.16"
 end
