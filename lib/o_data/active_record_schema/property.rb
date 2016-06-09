6fb89ac914c40dc3ebd7d954d10025ab4728734b
Sebastian Kliem <sebastian-kliem@live.de>
2016-06-09 15:22:04 +0200

    fix Edm.Decimal again

--- lib/o_data/active_record_schema/property.rb
+++ lib/o_data/active_record_schema/property.rb
@@ -39,7 +39,7 @@ module OData
       def value_for(one)
         v = one.send(@column_adapter.name.to_sym)
         return v.to_datetime.strftime('%Y-%m-%dT%H:%M:%S') if v.class == ActiveSupport::TimeWithZone
-        return v.to_s + "M" if return_type == 'Edm.Decimal'
+        return v.to_s if return_type == 'Edm.Decimal'
         v.respond_to?(:iso8601) ? v.send(:iso8601) : v
       end
     end
