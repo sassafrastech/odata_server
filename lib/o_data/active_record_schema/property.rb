0d5e25dfb5d08104fefb0f3b09ed884eb45188e5
Numa Claudel <numaclaudel@ymail.com>
2016-08-01 17:04:07 +0200

    feat (property): decimal type
    add decimal column type

--- lib/o_data/active_record_schema/property.rb
+++ lib/o_data/active_record_schema/property.rb
@@ -9,33 +9,34 @@ module OData
         :date      => 'Edm.Date',
         :datetime  => 'Edm.DateTime',
         :float     => 'Edm.Decimal',
+        :decimal   => 'Edm.Decimal',
         :integer   => 'Edm.Int32',
         :string    => 'Edm.String',
         :text      => 'Edm.String',
         :timestamp => 'Edm.DateTime',
         :time      => 'Edm.Time'
       }.freeze
-      
+
       def self.return_type_for(column_adapter)
         @@column_adapter_return_types[column_adapter.type]
       end
-      
+
       def self.name_for(column_adapter)
         column_adapter.name.to_s
       end
-      
+
       def self.nullable?(column_adapter)
         column_adapter.null
       end
-      
+
       attr_reader :column_adapter
-      
+
       def initialize(schema, entity_type, column_adapter)
         super(schema, entity_type, self.class.name_for(column_adapter), self.class.return_type_for(column_adapter), self.class.nullable?(column_adapter))
-        
+
         @column_adapter = column_adapter
       end
-      
+
       def value_for(one)
         v = one.send(@column_adapter.name.to_sym)
         return v.to_datetime.strftime('%Y-%m-%dT%H:%M:%S') if v.class == ActiveSupport::TimeWithZone
