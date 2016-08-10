015f997a0895899e93ad5f948928184790dc824e
Numa Claudel <numaclaudel@ymail.com>
2016-08-10 16:48:23 +0200

    feat (response): header
    add OData-Version header to the responses

--- app/controllers/o_data_controller.rb
+++ app/controllers/o_data_controller.rb
@@ -2,8 +2,6 @@ class ODataController < ApplicationController
   include ActionController::MimeResponds
   include ActionController::Helpers
 
-  skip_before_action :verify_authenticity_token, only: :options
-
   @@o_data_atom_xmlns = {
     "xmlns"   => "http://www.w3.org/2005/Atom",
     "xmlns:m" => "http://docs.oasis-open.org/odata/ns/metadata"
@@ -20,9 +18,12 @@ class ODataController < ApplicationController
   rescue_from OData::ODataException, :with => :handle_exception
   rescue_from ActiveRecord::RecordNotFound, :with => :handle_exception
 
+  skip_before_action :verify_authenticity_token, only: :options
+
   before_filter :extract_resource_path_and_query_string, :only => [:resource]
   before_filter :parse_resource_path_and_query_string!,  :only => [:resource]
   before_filter :set_request_format!,                    :only => [:resource]
+  after_action :set_header
 
   %w{service metadata resource}.each do |method_name|
     define_method(:"redirect_to_#{method_name}") do
@@ -128,6 +129,29 @@ class ODataController < ApplicationController
       # segment of the resource path.
       raise OData::Core::Errors::CoreException.new(@query)
     end
+  end
+
+  protected
+
+  def get_selected_properties_for(query, entity_type)
+    if select_option = query.options.find { |o| o.option_name == OData::Core::Options::SelectOption.option_name }
+      if select_option.entity_type == entity_type
+        # entity_type is the $select'ed collection/navigation property
+        return select_option.properties
+      else
+        # entity_type is an $expand'ed navigation property
+       if expand_option = query.options.find{ |o| o.option_name == OData::Core::Options::ExpandOption.option_name }
+         if expand_option.value.downcase == entity_type.plural_name.downcase
+           return entity_type.properties
+         end
+       else
+         return []
+       end
+      end
+    end
+
+    # $select option not supplied
+    entity_type.properties
   end
 
   private
@@ -342,26 +366,8 @@ class ODataController < ApplicationController
     end
   end
 
-  protected
-
-  def get_selected_properties_for(query, entity_type)
-    if select_option = query.options.find { |o| o.option_name == OData::Core::Options::SelectOption.option_name }
-      if select_option.entity_type == entity_type
-        # entity_type is the $select'ed collection/navigation property
-        return select_option.properties
-      else
-        # entity_type is an $expand'ed navigation property
-       if expand_option = query.options.find{ |o| o.option_name == OData::Core::Options::ExpandOption.option_name }
-         if expand_option.value.downcase == entity_type.plural_name.downcase
-           return entity_type.properties
-         end
-       else
-         return []
-       end
-      end
-    end
-
-    # $select option not supplied
-    entity_type.properties
+  def set_header
+    response.headers['OData-Version'] = '4.0'
   end
+
 end
