ab8b9f807e382098c0bafa39d1df6ca9214934db
James Myers <espmiii@gmail.com>
2017-06-27 14:03:21 -0400

    if decimal is nil return nil instead of casting to 0

--- lib/o_data/active_record_schema/property.rb
+++ lib/o_data/active_record_schema/property.rb
@@ -44,7 +44,7 @@ module OData
       def value_for(one)
         column_name = @column_adapter.is_a?(Symbol) ? @column_adapter : @column_adapter.name.to_sym
         v = one.send(column_name)
-        return v.to_f if return_type == 'Edm.Decimal'
+        return v.to_f if return_type == 'Edm.Decimal' && !v.nil?
         return v.iso8601 if v.respond_to?(:iso8601)
         v
       end
