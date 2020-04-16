f78de2a75548bc58085e870678ca3881ae445b5e
Kevin Cooper <cooperka92@gmail.com>
2020-04-16 14:28:39 -0700

    Add specs for resource rendering

--- spec/requests/active_record_request_spec.rb
+++ spec/requests/active_record_request_spec.rb
@@ -19,7 +19,8 @@ describe OData::ActiveRecordSchema::Base do
     def expect_output(expected)
       get(path)
       expect(response).to have_http_status(:ok)
-      expect(response.body).to eq(expected)
+      # Don't worry about trailing newlines.
+      expect(response.body.rstrip).to eq(expected.rstrip)
     end
 
     context "root" do
@@ -78,6 +79,69 @@ describe OData::ActiveRecordSchema::Base do
 
         it "renders as expected" do
           expect_output(file_fixture("metadata_after_hook.xml").read)
+        end
+      end
+    end
+
+    context "resource" do
+      let(:path) { "#{root}/ActiveFoos" }
+
+      before do
+        (1..2).each do |n|
+          create(:active_foo, name: "test #{n}")
+        end
+      end
+
+      it "renders as expected" do
+        expect_output({
+          "@odata.context": "http://www.example.com/odata/$metadata#ActiveFoos",
+          value: [
+            { Id: 1, Name: "test 1", CreatedAt: "2020-01-01T12:00:00Z", UpdatedAt: "2020-01-01T12:00:00Z" },
+            { Id: 2, Name: "test 2", CreatedAt: "2020-01-01T12:00:00Z", UpdatedAt: "2020-01-01T12:00:00Z" }
+          ]
+        }.to_json)
+      end
+
+      context "with feed hook" do
+        let(:options) do
+          {
+            transform_json_for_resource_feed: lambda do |json|
+              json[:value].push({ name: "Fake data" })
+              json
+            end
+          }
+        end
+
+        it "renders as expected" do
+          expect_output({
+            "@odata.context": "http://www.example.com/odata/$metadata#ActiveFoos",
+            value: [
+              { Id: 1, Name: "test 1", CreatedAt: "2020-01-01T12:00:00Z", UpdatedAt: "2020-01-01T12:00:00Z" },
+              { Id: 2, Name: "test 2", CreatedAt: "2020-01-01T12:00:00Z", UpdatedAt: "2020-01-01T12:00:00Z" },
+              { name: "Fake data" }
+            ]
+          }.to_json)
+        end
+      end
+
+      context "with entry hook" do
+        let(:options) do
+          {
+            transform_json_for_resource_entry: lambda do |json|
+              json["Name"] = "foo"
+              json
+            end
+          }
+        end
+
+        it "renders as expected" do
+          expect_output({
+            "@odata.context": "http://www.example.com/odata/$metadata#ActiveFoos",
+            value: [
+              { Id: 1, Name: "foo", CreatedAt: "2020-01-01T12:00:00Z", UpdatedAt: "2020-01-01T12:00:00Z" },
+              { Id: 2, Name: "foo", CreatedAt: "2020-01-01T12:00:00Z", UpdatedAt: "2020-01-01T12:00:00Z" },
+            ]
+          }.to_json)
         end
       end
     end
