fa2b9a16228f9ce21547948a43f60f1c6aa3f54f
Tom Smyth <tom@gather.coop>
2021-02-16 16:39:25 -0500

    Comment out insecure code

--- lib/o_data/active_record_schema/mixins/option_translator.rb
+++ lib/o_data/active_record_schema/mixins/option_translator.rb
@@ -2,31 +2,35 @@ module OData::ActiveRecordSchema::Mixins
   module OptionTranslator
 
     def translate_filter(scope, predicate, left, right)
-      if left.try(:left).present?
-        pre_scope = translate_filter(scope, left.value, left.left, left.right)
-        left = OpenStruct.new(value: pre_scope.constraints.first.to_sql)
-      end
-
-      left = left.value
-      right = right.value
-
-      case predicate.to_sym
-      when :eq then scope.where("#{left} = #{right}")
-      when :ne then scope.where("#{left} != #{right}")
-      when :gt then scope.where("#{left} > #{right}")
-      when :lt then scope.where("#{left} < #{right}")
-      when :ge then scope.where("#{left} >= #{right}")
-      when :le then scope.where("#{left} <= #{right}")
-      when :and then scope.where(left).where(right)
-      when :or then scope.where(left).or(right)
-      when :add then scope.where("#{left} + #{right}")
-      when :sub then scope.where("#{left} - #{right}")
-      when :mul then scope.where("#{left} * #{right}")
-      when :div then scope.where("#{left} / #{right}")
-      when :mod then scope.where("#{left} % #{right}")
-      else
-        scope
-      end
+      # The code in this method seems fundamentally insecure since it is dropping user
+      # input directly into a where clause without escaping it. If we decide to enable this functionality
+      # it will need to be carefully tested and likely refactored.
+      return scope
+      # if left.try(:left).present?
+      #   pre_scope = translate_filter(scope, left.value, left.left, left.right)
+      #   left = OpenStruct.new(value: pre_scope.constraints.first.to_sql)
+      # end
+      #
+      # left = left.value
+      # right = right.value
+      #
+      # case predicate.to_sym
+      # when :eq then scope.where("#{left} = #{right}")
+      # when :ne then scope.where("#{left} != #{right}")
+      # when :gt then scope.where("#{left} > #{right}")
+      # when :lt then scope.where("#{left} < #{right}")
+      # when :ge then scope.where("#{left} >= #{right}")
+      # when :le then scope.where("#{left} <= #{right}")
+      # when :and then scope.where(left).where(right)
+      # when :or then scope.where(left).or(right)
+      # when :add then scope.where("#{left} + #{right}")
+      # when :sub then scope.where("#{left} - #{right}")
+      # when :mul then scope.where("#{left} * #{right}")
+      # when :div then scope.where("#{left} / #{right}")
+      # when :mod then scope.where("#{left} % #{right}")
+      # else
+      #   scope
+      # end
     end
 
     def translate_limitator(scope, predicate, value)
